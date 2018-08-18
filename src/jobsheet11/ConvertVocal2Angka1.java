5332500693a8b413df6c8728e2b91f3a8256a51e
user <user@LAPTOP-47AR98S0>
2018-08-18 22:35:36 +0700

    Initial Commit

---
+++ src/jobsheet11/ConvertVocal2Angka1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+
+/**
+ *
+ * @author user
+ */
+public class ConvertVocal2Angka1 {
+      private static void tampilJudul(String identitas){
+        System.out.println("Identitas" +identitas);
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal ke Angka)\n");
+    }
+    public static void main(String[] args) {
+    String identitas = "Nurafiif Almas Azhari / XRPL5/ 33";
+    tampilJudul(identitas);
+    
+    }
+}
